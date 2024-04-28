d52b752f81c31911c42e47d775a4bb3371da974c
Maxime Pawlak <contact@maximepawlak.fr>
2024-04-28 21:32:18 +0200

    Update base URL to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "https://github.com/MaximePawlakFr/coin-vue/",
+  base: "https://maximepawlakfr.github.io/coin-vue/",
 });
