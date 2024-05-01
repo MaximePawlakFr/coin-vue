2276dc23c3f4fbb103e818082f0db22cd4ebc69e
Maxime Pawlak <contact@maximepawlak.fr>
2024-05-01 19:08:48 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,71 @@
 <script setup>
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import * as Plotly from "plotly.js-dist";
-import { runQuery } from './duckdb/plotly.js'
-import {useDailyDataStore} from './stores/dailyData'
-import fiches from "./assets/meteofrance-fiches-stations.js";
-import {  
-   stationsColumns, parametersColumns} from "./assets/meteofrance-columns";
-import perspective from "https://cdn.jsdelivr.net/npm/@finos/perspective/dist/cdn/perspective.js";
+import { ref } from "vue"
+import HelloWorld from "./components/HelloWorld.vue"
+import * as Plotly from "plotly.js-dist"
+import { runQuery } from "./duckdb/plotly.js"
+import { useDailyDataStore } from "./stores/dailyData"
+import fiches from "./assets/meteofrance-fiches-stations.js"
+import { stationsColumns, parametersColumns } from "./assets/meteofrance-columns"
+import perspective from "https://cdn.jsdelivr.net/npm/@finos/perspective/dist/cdn/perspective.js"
 
-import posthog from 'posthog-js'
-const ENV = import.meta.env;
+import posthog from "posthog-js"
+const ENV = import.meta.env
 
-const POSTHOG_KEY = ENV.VITE_POSTHOG_KEY 
-console.log(ENV) 
+const POSTHOG_KEY = ENV.VITE_POSTHOG_KEY
+console.log(ENV)
 
 // Init only for prod to avoir sending false signals
-if(POSTHOG_KEY){
-  posthog.init(POSTHOG_KEY, { api_host: 'https://eu.i.posthog.com' })
+if (POSTHOG_KEY) {
+  posthog.init(POSTHOG_KEY, { api_host: "https://eu.i.posthog.com" })
 }
 
-
-const worker = perspective.worker();
+const worker = perspective.worker()
 
 const graph = ref(null)
 const viewer = ref(null)
-const dailyDataStore  = useDailyDataStore()
+const dailyDataStore = useDailyDataStore()
 
-const isDrawing = defineModel('isDrawing', false);
+const isFetchingData = defineModel("isFetchingData", false)
+const isGraphReady = defineModel("isGraphReady", false)
 
 function submit(form) {
-  posthog.capture('my event', { property: 'value' })
+  posthog.capture("my event", { property: "value" })
   console.log("submit", form)
-  isDrawing.value = true;
-  return runQuery(form)
-  .then(res => {
+  isFetchingData.value = true
+  return runQuery(form).then((res) => {
     console.log({ res })
-    const { dates, data } = res;
-    
- const workerData = worker.table(data)
-    viewer.value.load(workerData);
+    const { dates, data } = res
 
-    
-    draw(graph.value, dates, data);
-  isDrawing.value = false;
+    const workerData = worker.table(data)
+    viewer.value.load(workerData)
 
+    draw(graph.value, dates, data)
+    isGraphReady.value = true
+    isFetchingData.value = false
   })
 }
 
 const draw = (div, dates, data) => {
-  console.log({data})
-  const dataKeys =  Object.keys(data)
-  const plotlyData = dataKeys.map(column => {
-    if(column !== "AAAAMMJJ"){
-      return {
-        name: column,
-        x:dates,
-        y:data[column],
-        mode:"lines"
+  console.log({ data })
+  const dataKeys = Object.keys(data)
+  const plotlyData = dataKeys
+    .map((column) => {
+      if (column !== "AAAAMMJJ") {
+        return {
+          name: column,
+          x: dates,
+          y: data[column],
+          mode: "lines"
+        }
       }
-    }
-  }).filter(item => item)
-  console.log({plotlyData})
+    })
+    .filter((item) => item)
+  console.log({ plotlyData })
 
   Plotly.newPlot(
-    div,plotlyData,
-    
+    div,
+    plotlyData,
+
     {
       title: "Coin Coin",
       xaxis: {
@@ -77,91 +77,74 @@ const draw = (div, dates, data) => {
               count: 1,
               label: "1m",
               step: "month",
-              stepmode: "backward",
+              stepmode: "backward"
             },
             {
               count: 6,
               label: "6m",
               step: "month",
-              stepmode: "backward",
+              stepmode: "backward"
             },
-            { step: "all" },
-          ],
+            { step: "all" }
+          ]
         },
-        rangeslider: { range: [dates[0], dates[dates.length-1]] },
-        type: "date",
+        rangeslider: { range: [dates[0], dates[dates.length - 1]] },
+        type: "date"
       },
       showLegend: true,
       scrollZoom: true,
-      displaylogo: false,
-    },
-  );
+      displaylogo: false
+    }
+  )
 }
-const stationsNames = [];
+const stationsNames = []
 const stationsIds = []
 
-const stations = fiches.features.map(feature => {
-  const {NOM_USUEL, NUM_POSTE, NUM_DEP} = feature.properties
+const stations = fiches.features.map((feature) => {
+  const { NOM_USUEL, NUM_POSTE, NUM_DEP } = feature.properties
   stationsNames.push(NOM_USUEL)
   stationsIds.push(NUM_POSTE)
 
   const station = {
-    name:NOM_USUEL,
-    id:NUM_POSTE,
-    department:NUM_DEP>9?""+NUM_DEP:"0"+NUM_DEP
-  };
+    name: NOM_USUEL,
+    id: NUM_POSTE,
+    department: NUM_DEP > 9 ? "" + NUM_DEP : "0" + NUM_DEP
+  }
   return station
 })
-stations.sort((a,b)=>{
-  if( a.NUM_DEP >= b.NUM_DEP){
-    return 1;
-  }else{
+stations.sort((a, b) => {
+  if (a.NUM_DEP >= b.NUM_DEP) {
+    return 1
+  } else {
     return -1
   }
 })
-dailyDataStore.setStations(stations);
+dailyDataStore.setStations(stations)
 dailyDataStore.setStationsColumns(stationsColumns)
 dailyDataStore.setParametersColumns(parametersColumns)
 dailyDataStore.setStationsNames(stationsNames.sort())
 dailyDataStore.setStationsIds(stationsIds.sort())
-
 </script>
 
 <template>
-  <header>
-    <h1 class="text-3xl text-center m-16">Meteo 🦆 Coin-Coin 🦆</h1>
+  <header class="">
+    <h1 class="text-3xl text-center">Meteo 🦆 Coin-Coin 🦆</h1>
   </header>
-  <main>
-    <div>
+  <main class="flex-1 container mx-auto">
+    <div class="">
       <HelloWorld msg="You did it!" @submit="submit" />
-      <h3>isDrawing: {{ isDrawing }}</h3>
-      <perspective-viewer ref="viewer"> </perspective-viewer>
-      <div ref="graph" style="width: 100vw;height:400px;">
+
+      <div v-show="isGraphReady" class="flex flex-col">
+        <div ref="graph" class="h-80"></div>
+        <perspective-viewer ref="viewer" class="h-80"> </perspective-viewer>
       </div>
+      <h3 v-if="isFetchingData">isFetchingData: {{ isFetchingData }}</h3>
     </div>
-
   </main>
-  <footer class="flex flex-col items-center justify-center">
+  <footer class="flex flex-col items-center justify-center text-sm my-2">
     <div>
-      © All rights Reserverd - Meteo Coin-Coin by Maxime Pawlak 
+      <span :title="ENV.VITE_BUILD_DATE" class="text-sm"> v{{ ENV.VITE_APP_VERSION }} </span>
     </div>
-      <div>
-        
-      <span :title="ENV.VITE_BUILD_DATE" class="text-sm">
-        v{{ENV.VITE_APP_VERSION}}
-      </span>  
-    </div>
-    <div>
-      <span class="text-xs">
-        {{ENV.VITE_BUILD_DATE}}
-      </span>
-    </div>
+    <div>© All rights Reserved - Meteo Coin-Coin by Maxime Pawlak</div>
   </footer>
-
 </template>
-
-<style scoped>
-perspective-viewer {
-  width: 100vw;
-  height: 200px;
-}</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {useDailyDataStore} from '../stores/dailyData'
-import { storeToRefs } from 'pinia'
-import CONSTANTS from '../constants'
+import { useDailyDataStore } from "../stores/dailyData"
+import { storeToRefs } from "pinia"
+import CONSTANTS from "../constants"
 import sqlClient from "../duckdb/sqlClient"
 
 defineProps({
@@ -11,39 +11,33 @@ defineProps({
   }
 })
 const dailyDataStore = useDailyDataStore()
-const { 
-  stations,
-    stationsNames,
-    parametersColumns,
-    stationsColumns,
-    stationsIds } = storeToRefs(dailyDataStore)
+const { stations, stationsNames, parametersColumns, stationsColumns, stationsIds } =
+  storeToRefs(dailyDataStore)
 
-    const parquetFilesUrls = CONSTANTS.DAILY_DATA.PARQUET_FILES_URLS
-console.log({parquetFilesUrls})
+const parquetFilesUrls = CONSTANTS.DAILY_DATA.PARQUET_FILES_URLS
+console.log({ parquetFilesUrls })
 
 const defaultStartDate = new Date()
-defaultStartDate.setDate(defaultStartDate.getDate() - 365);
+defaultStartDate.setDate(defaultStartDate.getDate() - 365)
 
-const defaultStartDateStr = defaultStartDate.toISOString().slice(0, 10);//{...defaultStartDate.toISOString().slice(0, 10)};
-const defaultEndDateStr = new Date().toISOString().slice(0, 10);
+const defaultStartDateStr = defaultStartDate.toISOString().slice(0, 10) //{...defaultStartDate.toISOString().slice(0, 10)};
+const defaultEndDateStr = new Date().toISOString().slice(0, 10)
 
-
-const formParametersColumns = defineModel('formParametersColumns',{
-  default:[]
+const formParametersColumns = defineModel("formParametersColumns", {
+  default: []
 })
-const formStationName = defineModel('formStationName',{
-  default:null
+const formStationName = defineModel("formStationName", {
+  default: null
 })
-const formStartDate = defineModel('formStartDate',{
-  default:null
+const formStartDate = defineModel("formStartDate", {
+  default: null
 })
-const formEndDate = defineModel('formEndDate',{
-  default:null
+const formEndDate = defineModel("formEndDate", {
+  default: null
 })
 
-formStartDate.value = defaultStartDateStr;
-formEndDate.value = defaultEndDateStr;
-
+formStartDate.value = defaultStartDateStr
+formEndDate.value = defaultEndDateStr
 
 const emit = defineEmits({
   submit: (form) => {
@@ -56,55 +50,67 @@ function onSubmit() {
   console.log("onSubmit")
   const defaultColumns = ["AAAAMMJJ"]
   const columns = defaultColumns.concat(formParametersColumns.value)
-  const columnsStr = columns.join(", ");
+  const columnsStr = columns.join(", ")
   const filesStr = sqlClient.getUrlsArrayForSQLQuery(parquetFilesUrls)
-  console.log({filesStr})
-  const stationNameWhere = `NOM_USUEL='${formStationName.value}'`;
-  const datesWhere = `AAAAMMJJ>=${formStartDate.value.replaceAll("-","")} AND AAAAMMJJ<=${formEndDate.value.replaceAll("-","")}`;
-  
+  console.log({ filesStr })
+  const stationNameWhere = `NOM_USUEL='${formStationName.value}'`
+  const datesWhere = `AAAAMMJJ>=${formStartDate.value.replaceAll("-", "")} AND AAAAMMJJ<=${formEndDate.value.replaceAll("-", "")}`
+
   const fullQuery = `SELECT ${columnsStr} from read_parquet(${filesStr}) WHERE ${stationNameWhere} AND ${datesWhere} ORDER BY AAAAMMJJ`
   console.log(fullQuery)
 
-  emit('submit', { query: fullQuery, columns:columns })
+  emit("submit", { query: fullQuery, columns: columns })
 }
 </script>
 
 <template>
-  <div class="container mx-auto">
-    
+  <div>
     <form @submit.prevent="onSubmit" class="">
       <div>
-       <p>Parameters:</p><button type="button">All</button><button type="button">None</button>
-       <fieldset class="flex flex-wrap">
-          <template v-for="item in parametersColumns">
+        <p>Parameters:</p>
+        <button type="button">All</button><button type="button">None</button>
+        <fieldset class="flex flex-wrap">
+          <template v-for="item in parametersColumns" :key="item">
             <div class="mx-1">
-
-              <input type="checkbox" name="formParametersColumns" :id="item" :value="item" v-model="formParametersColumns"/>
+              <input
+                type="checkbox"
+                name="formParametersColumns"
+                :id="item"
+                :value="item"
+                v-model="formParametersColumns"
+              />
               <label :for="item" class="mx-2">{{ item }}</label>
             </div>
           </template>
-
         </fieldset>
       </div>
 
       <fieldset>
         <label for="stationsNames">Station: </label>
-        <input type="text" v-model="formStationName" name="stationsNames" id="stationsNames" list="stationsNamesList"/>
-          <datalist id="stationsNamesList">
-            
-            <option v-for="item in stations" :value="item.name" :key="item.id"> {{ item.department }} - {{item.name}}</option>
-          </datalist>
+        <input
+          type="text"
+          v-model="formStationName"
+          name="stationsNames"
+          id="stationsNames"
+          list="stationsNamesList"
+          autocomplete="off"
+        />
+        <datalist id="stationsNamesList">
+          <option v-for="item in stations" :value="item.name" :key="item.id">
+            {{ item.department }} - {{ item.name }}
+          </option>
+        </datalist>
       </fieldset>
 
       <div class="flex">
         <fieldset>
           <label for="">From</label>
-          <input type="date" v-model="formStartDate"/>
+          <input type="date" v-model="formStartDate" />
         </fieldset>
 
         <fieldset>
-          <label for="">To</label>  
-          <input type="date" v-model="formEndDate"/>
+          <label for="">To</label>
+          <input type="date" v-model="formEndDate" />
         </fieldset>
       </div>
 
@@ -112,4 +118,3 @@ function onSubmit() {
     </form>
   </div>
 </template>
-
